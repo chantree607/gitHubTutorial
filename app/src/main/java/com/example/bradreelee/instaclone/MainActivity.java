6454d5d3ea7d98fac0c108c219d3a7ba243ff962
chantree607 <chantree607@naver.com>
2019-04-02 19:42:09 +0200

    first commmit

---
+++ app/src/main/java/com/example/bradreelee/instaclone/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.bradreelee.instaclone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button profileBtn;
+    Button idBtn;
+    Button commentBtn;
+    Button shareBtn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        profileBtn = (Button) findViewById(R.id.profile_btn);
+        idBtn = (Button) findViewById(R.id.id_btn);
+        commentBtn = (Button) findViewById(R.id.comment_btn);
+        shareBtn = (Button) findViewById(R.id.share_btn);
+
+
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "clilcekd profile",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        idBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "clicked userID", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        commentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "this is a comment function", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "this is a share function", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
